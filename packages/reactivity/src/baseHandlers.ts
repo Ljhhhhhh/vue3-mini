bb031c7f748aaad2b4bc3b2eaaa52ac4498a022b
lujiehui <13216698987@163.com>
2023-01-14 20:18:15 +0800

    reactive 模块

---
+++ packages/reactivity/src/baseHandlers.ts
@@ -0,0 +1,37 @@
+import { track, trigger } from './effect'
+
+const get = createGetter()
+
+function createGetter() {
+  return function get(target: object, key: string | symbol, receiver: object) {
+    // 利用 Reflect.get 获取新值
+    const res = Reflect.get(target, key, receiver)
+    // 收集依赖
+    track(target, key)
+    return res
+  }
+}
+
+const set = createSetter()
+
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ) {
+    // 利用 Reflect.set 设置新值
+    const result = Reflect.set(target, key, value, receiver)
+    // 触发依赖
+    trigger(target, key)
+    return result
+  }
+}
+/**
+ * 响应性的 handler
+ */
+export const mutableHandlers: ProxyHandler<object> = {
+  get,
+  set
+}
---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,106 @@
+import { createDep, Dep } from './dep'
+
+/**
+ * effect 函数
+ * @param fn 执行方法
+ * @returns 以 ReactiveEffect 实例为 this 的执行函数
+ */
+export function effect<T = any>(fn: () => T) {
+  // 生成 ReactiveEffect 实例
+  const _effect = new ReactiveEffect(fn)
+  // 执行 run 函数
+  _effect.run()
+}
+
+/**
+ * 单例的，当前的 effect
+ */
+export let activeEffect: ReactiveEffect | undefined
+
+/**
+ * 响应性触发依赖时的执行类
+ */
+export class ReactiveEffect<T = any> {
+  constructor(public fn: () => T) {}
+
+  run() {
+    // 为 activeEffect 赋值
+    activeEffect = this
+
+    // 执行 fn 函数
+    return this.fn()
+  }
+}
+
+type KeyToDepMap = Map<any, Dep>
+/**
+ * 收集所有依赖的 WeakMap 实例：
+ * 1. `key`：响应性对象
+ * 2. `value`：`Map` 对象
+ * 		1. `key`：响应性对象的指定属性
+ * 		2. `value`：指定对象的指定属性的 执行函数
+ */
+const targetMap = new WeakMap<any, KeyToDepMap>()
+
+/**
+ * 用于收集依赖的方法
+ * @param target WeakMap 的 key
+ * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+ */
+export function track(target: object, key: unknown) {
+  // 如果当前不存在执行函数，则直接 return
+  if (!activeEffect) return
+  let depsMap = targetMap.get(key)
+  if (!depsMap) {
+    targetMap.set(target, (depsMap = new Map()))
+  }
+  // 获取指定 key 的 dep
+  let dep = depsMap.get(key)
+  // 如果 dep 不存在，则生成一个新的 dep，并放入到 depsMap 中
+  if (!dep) {
+    depsMap.set(key, (dep = createDep()))
+  }
+
+  trackEffects(dep)
+}
+
+/**
+ * 利用 dep 依次跟踪指定 key 的所有 effect
+ * @param dep
+ */
+export function trackEffects(dep: Dep) {
+  dep.add(activeEffect!)
+}
+
+/**
+ * 触发依赖的方法
+ * @param target WeakMap 的 key
+ * @param key 代理对象的 key，当依赖被触发时，需要根据该 key 获取
+ */
+export function trigger(target: object, key?: unknown) {
+  // 依据 target 获取存储的 map 实例
+  const depsMap = targetMap.get(target)
+  // 如果 map 不存在，则直接 return
+  if (!depsMap) {
+    return
+  }
+
+  let dep: Dep | undefined = depsMap.get(key)
+  if (!dep) {
+    return
+  }
+
+  triggerEffects(dep)
+}
+
+export function triggerEffects(dep: Dep) {
+  // 把 dep 构建成一个数组
+  const effects = Array.isArray(dep) ? dep : [dep]
+  effects.forEach(effect => {
+    triggerEffect(effect)
+  })
+}
+
+export function triggerEffect(effect: ReactiveEffect) {
+  effect.run()
+}
